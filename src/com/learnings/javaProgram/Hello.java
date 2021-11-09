33e675dfe2cca614d283a7cc10078ce53dc019de
Ankit Raj Prasad <prasad.ankitarj@gmail.com>
2021-11-09 22:08:53 +0530

    Initial commit

---
+++ src/com/learnings/javaProgram/Hello.java
@@ -0,0 +1,18 @@
+package com.learnings.javaProgram;
+/* =================================
+
+author ankitrajprasad created on 09/11/21 
+inside the package - com.learnings.javaProgram 
+
+=====================================*/
+
+
+public class Hello {
+    //Access Modifier : Public.
+    // static : JVM to find the stack block to know which package of code to run.
+    //void : wont retrun anything.
+    public static void main(String[] args) {
+        System.out.println("Learning Core Java Again :) ");
+
+    }
+}
